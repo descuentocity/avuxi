4b84c161c3048d7675736ec3d3f8fdb06c7f9475
matiascarreras <mcarreras@descuentocity.com>
2022-05-19 15:53:56 -0300

    prueba avuxi con leaflet

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # avuxi
+### install
+
+```
+npm i --save avuxi/vue-avapi
+```
+
+### usage
+
+```js
+import Vue from 'vue'
+import vueAvapi from 'vue-avapi'
+
+Vue.use(vueAvapi)
+```
+
+```html
+<!-- include Leaflet script -->
+<!-- include configuration script from https://admin.avuxi.com -->
+
+<avapi-map confId='5ae6df87a93f4e1e02a6122a'
+           lat='41.38605'
+           lng='2.17014'
+           zoom='13'>
+</avapi-map>
+
+<avapi-lse confId='5ae6e03da93f4e1e02a6122b'
+           lat='41.390847'
+           lng='2.16902'>
+</avapi-lse>
+```
---
+++ src/map.js
@@ -0,0 +1,24 @@
+export default {
+  name: 'avapiMap',
+  props: ['confId', 'lat', 'lng', 'zoom'],
+  template: `<div class='vxmap'></div>`,
+  
+  mounted() {
+    const leaflet = require('leaflet');
+
+    const options = {
+      zoom: parseInt(this.zoom),
+      minZoom: 10,
+      maxZoom: 18,
+      zoomControl: true,
+      center: { lat: this.lat, lng: this.lng },
+    }
+
+    const map = leaflet.map(this.$el, options);
+    leaflet.tileLayer('https://{s}-tile.avuxicdn.com/avuxi-th2/{z}/{x}/{y}.png?tpkey=622bb407a904e838e65ebfd8', {
+      attribution: '<a href="https://www.avuxi.com" target="_blank">AVUXI</a>',
+    }).addTo(map);
+
+    AVUXI.MapStart(map, this.confId);
+  },
+}
